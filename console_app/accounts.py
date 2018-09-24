48a22675f085d50f4dc27600f10a19e3af945c72
myco <coditmyco6@gmail.com>
2018-09-24 13:14:14 +0300

    Add accounts.py file [Finishes #160716812]

---
+++ console_app/accounts.py
@@ -0,0 +1,24 @@
+class User(object):
+    """Class that defines attributes for a User object."""
+
+    accounts = {}
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def add_account(cls, username, email, password):
+        user = User(username=username, email=email, password=password)
+        cls.accounts[username] = user
+        print(str(cls.accounts[username].email))
+
+
+    @classmethod
+    def login(cls, username):
+        try:
+            cls.accounts[username]
+            return True
+        except KeyError as e:
+            return e
